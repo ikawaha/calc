dbe89f5be78a83abe504e98f570cd134e3c81e7f
ikawaha <ikawaha@users.noreply.github.com>
2023-10-14 09:58:45 +0900

    initial commit

---
+++ calc.go
@@ -0,0 +1,24 @@
+package calc
+
+import (
+	"context"
+	"log"
+
+	calcsvc "github.com/ikawaha/basic/gen/calc"
+)
+
+// calc service example implementation.
+// The example methods log the requests and return zero values.
+type calcSvc struct {
+	logger *log.Logger
+}
+
+// NewCalc returns the calc service implementation.
+func NewCalc(logger *log.Logger) calcsvc.Service {
+	return &calcSvc{logger}
+}
+
+// Multiply implements multiply.
+func (s *calcSvc) Multiply(ctx context.Context, p *calcsvc.MultiplyPayload) (int, error) {
+	return p.A * p.B, nil
+}
